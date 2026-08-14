17c4474f36c32eacaa67b8ced3c380d716ef65dc
mriamali2001 <mriamali2001@gmail.com>
2026-08-15 01:56:15 +0300

    first commit

---
+++ src/booking.ts
@@ -0,0 +1,7 @@
+let name: string = "Mariam";
+let age: number = 25;
+let isStudent: boolean = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
---
+++ tests/booking.test.ts
@@ -0,0 +1,17 @@
+let name: string = "Student";
+let age: number = 25;
+let isStudent: boolean = true;
+
+let country: string = "Iraq";
+let year: number = 2026;
+let favoriteLanguage: string = "TypeScript";
+
+console.log("=== User Information ===");
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
+console.log("Country:", country);
+console.log("Year:", year);
+console.log("Favorite Language:", favoriteLanguage);
+
+console.log("Hello", name + "!");
