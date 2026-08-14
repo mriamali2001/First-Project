259923d1b5d912e4bffddb8f2cd8da3dd70b33f8
mriamali2001 <mriamali2001@gmail.com>
2026-08-15 02:35:33 +0300

    New value added

--- src/booking.ts
+++ src/booking.ts
@@ -1,7 +1,9 @@
 let name: string = "Mariam";
+let University: string = "Hilla";
 let age: number = 25;
 let isStudent: boolean = true;
 
 console.log("Name:", name);
+console.log("University:", University);
 console.log("Age:", age);
 console.log("Student:", isStudent);
